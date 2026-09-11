8c3a359d87f00e1113d2b58985e8b7ebe9e5e7fe
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 08:32:12 +0100

    Add Vercel Web Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./app.css";
 import "./tokens.css";
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <div data-brand="calabi">
         <App />
+        <Analytics />
       </div>
     </BrowserRouter>
   </React.StrictMode>,
